78b0c40b050866c89ec4ac7711cd5e015c04ce1f
jiakun <aqeja@outlook.com>
2023-09-28 14:45:17 +0800

    fix(auth): auth header

--- api/auth.js
+++ api/auth.js
@@ -2,18 +2,10 @@ const { OAuth2Client } = require("google-auth-library");
 const oAuth2Client = new OAuth2Client(process.env.CLIENT_ID, process.env.CLIENT_SECRET, "postmessage");
 
 export default async function handler(req, res) {
-  if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    return res.status(200);
-  }
-  if (req.method === "POST") {
-    const { body } = req;
-    // return res.send(`Hello ${body.name}, you just parsed the request body!`);
-    const { tokens } = await oAuth2Client.getToken(body.code);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST");
-    res.status(200).json(tokens);
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
-  }
+  const { body } = req;
+  // return res.send(`Hello ${body.name}, you just parsed the request body!`);
+  const { tokens } = await oAuth2Client.getToken(body.code);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.status(200).json(tokens);
 }
